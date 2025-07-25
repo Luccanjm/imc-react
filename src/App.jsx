c1f8eb17ced7d3609de68f48432fbff9319ba82c
Lucca Martinelli <lucnjm@gmail.com>
2025-07-25 20:56:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form"
+
+function App() {
+
+
+  return (
+   <>
+    <h1>IMC</h1>
+    <Form />
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import {useState} from "react"
+
+const Form = () => {
+    let [altura, setAltura] = useState(0);
+    let [peso, setPeso] = useState(0);
+    let alturaEmMetros = altura / 100
+    let imc = peso / (alturaEmMetros * alturaEmMetros)
+    let classificacao = "Não classificado"
+    if(imc < 18.5){
+        classificacao = "Abaixo do peso"
+    } else if(imc < 24.9 && imc > 18.5){
+        classificacao = "Peso Normal"
+    }else if(imc < 29.9 && imc > 24.9){
+       classificacao = "Sobrepeso"
+    }else{
+        classificacao = "Obesidade"
+    }
+
+    return(
+        <form>
+            <label htmlFor="">Altura</label>
+            <input type="number" onChange={evento => setAltura(evento.target.value)}/>
+            <label htmlFor="">Peso</label>
+            <input type="number" onChange={evento => setPeso(evento.target.value)}/> 
+            <button type="submit">Calcular</button>
+            <h2>Peso: {peso}</h2>
+            <h2>Altura: {altura}</h2>
+            <h2>IMC: {imc}</h2>
+            <h2>Classificação: {classificacao}</h2>
+        </form>
+    )
+}
+
+export default Form

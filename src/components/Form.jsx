36322a77f12b86113f91c757cd2bb74f3dc905a4
Lucca Martinelli <lucnjm@gmail.com>
2025-07-26 18:20:39 -0300

    update files

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,35 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 const Form = () => {
     let [altura, setAltura] = useState(0);
     let [peso, setPeso] = useState(0);
-    let alturaEmMetros = altura / 100
-    let imc = peso / (alturaEmMetros * alturaEmMetros)
-    let classificacao = "Não classificado"
-    if(imc < 18.5){
-        classificacao = "Abaixo do peso"
-    } else if(imc < 24.9 && imc > 18.5){
-        classificacao = "Peso Normal"
-    }else if(imc < 29.9 && imc > 24.9){
-       classificacao = "Sobrepeso"
-    }else{
-        classificacao = "Obesidade"
+    let [imc, setImc] = useState(0);
+    let [alturaEmMetros, setAlturaEmMetros] = useState(0);
+    let [classificacao, setClassificacao] = useState('');
+
+useEffect(() => {
+    setPeso(Number(peso));
+    setAltura(Number(altura));
+    setAlturaEmMetros(altura/100);
+    if(typeof peso === 'number' && typeof alturaEmMetros === 'number'){
+        setImc(peso / (alturaEmMetros * alturaEmMetros))
+
+        if(imc <= 0){
+            setClassificacao('Não classificado');
+        } else if(imc <= 18.5){
+            setClassificacao('Abaixo do peso');
+        } else if(imc <= 24.9 && imc > 18.5){
+            setClassificacao('Peso Normal');
+        }else if(imc <= 29.9 && imc > 24.9){
+            setClassificacao('Sobrepeso');
+        }else{
+            setClassificacao('Obesidade');
+        }
+    } else{
+        setImc(null);
     }
+   
+}, [peso,altura])
 
     return(
         <form>
